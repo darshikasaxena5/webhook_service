2f3a0d782abe105d53b814a7fb729427602d0438
Darshika Saxena <darshika13517@gmail.com>
2025-04-26 21:13:55 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.endpoints import subscriptions, ingestion, status
+from .api.endpoints import subscriptions, ingestion, status
 
 app = FastAPI(
     title="Webhook Delivery Service",
