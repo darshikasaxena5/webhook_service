6e54c8a309f5aded372fbcd180bddce78e69c603
Darshika Saxena <darshika13517@gmail.com>
2025-04-27 10:20:25 +0530

    c
    c

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: 'standalone',
-  // Force all URL resources to use HTTPS protocol
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://' : undefined,
   // Ensure the application always uses HTTPS
   headers: async () => {
     return [
